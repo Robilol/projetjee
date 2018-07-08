23190f52883db9131a80594bc6ba89297fcf5d23
Robilol <pimous78920@gmail.com>
2018-07-08 15:13:48 +0200

    add stats and chart JS

--- src/db/ClicDAO.java
+++ src/db/ClicDAO.java
@@ -1,10 +1,10 @@
 package db;
 
 import entities.ClicEntity;
+import entities.UrlEntity;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ClicDAO {
 
@@ -37,4 +37,39 @@ public class ClicDAO {
         return clics;
     }
 
+    public List<ClicEntity> findByUrl(Integer urlId) {
+        List<ClicEntity > clics = new ArrayList<ClicEntity>();
+
+        this.em.getTransaction().begin();
+        Query q = this.em.createQuery("SELECT v FROM ClicEntity v WHERE v.urlId = :urlId", ClicEntity.class).setParameter("urlId", urlId);
+
+        try {
+            clics = q.getResultList();
+        } catch (NoResultException e) {
+            System.out.println(e.getMessage());
+        }
+        this.em.getTransaction().commit();
+        return clics;
+    }
+
+    public Map<String, Integer> getStatsById(Integer urlId) {
+        HashMap<String, Integer> stats = new HashMap<String, Integer>();
+        this.em.getTransaction().begin();
+        Query q = this.em.createQuery("SELECT COUNT(v.id), v.date as bla  FROM ClicEntity v WHERE v.urlId = :urlId GROUP BY bla", ClicEntity.class).setParameter("urlId", urlId);
+        try {
+            List<Object[]> results = q.getResultList();
+
+            for (Object[] obj : results) {
+                stats.put(obj[1].toString(), Integer.parseInt(obj[0].toString()));
+            }
+
+        } catch (NoResultException e) {
+            System.out.println(e.getMessage());
+        }
+        this.em.getTransaction().commit();
+
+        Map<String, Integer> sortedMap = new TreeMap<String, Integer>(stats);
+
+        return sortedMap;
+    }
 }
